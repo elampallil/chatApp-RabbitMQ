20aa485a66432175e1fe9a4fa653cfdb2bf65932
Asus <you@example.com>
2020-05-31 01:13:03 +0530

    commit

---
+++ src/main/java/com/chatApplication/chatApp/ChatAppApplication.java
@@ -0,0 +1,14 @@
+package com.chatApplication.chatApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatAppApplication.class, args);
+		System.out.println("welcome");
+	}
+
+}
